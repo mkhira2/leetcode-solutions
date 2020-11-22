1065624990fba6f47f24a864cbc50577d0de4caf
Kenji Hirabayashi <mkhira2@gmail.com>
2020-11-22 14:15:41 -0600

    top k frequent elements

---
+++ javascript/medium/347-top-k-frequent-elements.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let hash = {};
+
+    for (num of nums) {
+        if (!hash[num]) {
+            hash[num] = 0
+        }
+        hash[num]++
+    }
+
+    const hashToArray = Object.entries(hash);
+    const sortedArray = hashToArray.sort((a, b) => b[1] - a[1]);
+    const sortedElements = sortedArray.map(num => num[0]);
+    return sortedElements.slice(0, k);
+};
+
+/*
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+You can return the answer in any order.
+*/
